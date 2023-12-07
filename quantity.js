018729eded32816d2ef4244e7a14fb9c1e275a3e
Tim Hacker <tim.hacker@madetech.com>
2023-12-07 15:26:46 +0000

    Reformat to 2 spaces - autoformat

--- quantity.js
+++ quantity.js
@@ -1,20 +1,23 @@
 class Quantity {
-    constructor(value, unit) {
-        this.value = value
-        this.unit = unit
-    }
+  constructor(value, unit) {
+    this.value = value;
+    this.unit = unit;
+  }
 
-    isEqual(other) {
-        let otherAmountInBaseUnits = other.unit.getBaseUnits(other.value);
-        let thisAmountInBaseUnits = this.unit.getBaseUnits(this.value);
-        return otherAmountInBaseUnits === thisAmountInBaseUnits;
-    }
+  isEqual(other) {
+    let otherAmountInBaseUnits = other.unit.getBaseUnits(other.value);
+    let thisAmountInBaseUnits = this.unit.getBaseUnits(this.value);
+    return otherAmountInBaseUnits === thisAmountInBaseUnits;
+  }
 
-    add(other) {
-        let otherEquivalentAmount = this.unit.getConvertedAmount(other.unit, other.value);
-        let newAmount = this.value + otherEquivalentAmount;
-        return new Quantity(newAmount, this.unit);
-    }
+  add(other) {
+    let otherEquivalentAmount = this.unit.getConvertedAmount(
+      other.unit,
+      other.value
+    );
+    let newAmount = this.value + otherEquivalentAmount;
+    return new Quantity(newAmount, this.unit);
+  }
 }
 
-module.exports = {Quantity}
+module.exports = { Quantity };
--- unit.js
+++ unit.js
@@ -1,34 +1,41 @@
 class Unit {
-    constructor(relativeUnitMultiplier, relativeUnit = null) {
-        this.relativeUnitMultiplier = relativeUnitMultiplier
-        this.baseUnitMultiplier = this.relativeUnitMultiplier * (relativeUnit?.baseUnitMultiplier ?? 1)
-        this.relativeUnit = relativeUnit
-    }
+  constructor(relativeUnitMultiplier, relativeUnit = null) {
+    this.relativeUnitMultiplier = relativeUnitMultiplier;
+    this.baseUnitMultiplier =
+      this.relativeUnitMultiplier * (relativeUnit?.baseUnitMultiplier ?? 1);
+    this.relativeUnit = relativeUnit;
+  }
 
-    getBaseUnits(amount) {
-        return amount * this.baseUnitMultiplier;
-    }
+  getBaseUnits(amount) {
+    return amount * this.baseUnitMultiplier;
+  }
 
-    getConvertedAmount(otherUnit, otherValue) {
-        return otherValue * otherUnit.baseUnitMultiplier / this.baseUnitMultiplier;
-    }
+  getConvertedAmount(otherUnit, otherValue) {
+    return (
+      (otherValue * otherUnit.baseUnitMultiplier) / this.baseUnitMultiplier
+    );
+  }
 }
 
+const TEASPOON = new Unit(1);
+const TABLESPOON = new Unit(3, TEASPOON);
+const OUNCE = new Unit(2, TABLESPOON);
+const CUP = new Unit(8, OUNCE);
+const PINT = new Unit(2, CUP);
+const QUART = new Unit(2, PINT);
+const GALLON = new Unit(4, QUART);
 
-const TEASPOON = new Unit(1)
-const TABLESPOON = new Unit(3, TEASPOON)
-const OUNCE = new Unit(2, TABLESPOON)
-const CUP = new Unit(8, OUNCE)
-const PINT = new Unit(2, CUP)
-const QUART = new Unit(2, PINT)
-const GALLON = new Unit(4, QUART)
+const INCH = new Unit(1);
+const FOOT = new Unit(12, INCH);
 
-const INCH = new Unit (1)
-const FOOT = new Unit(12, INCH)
-
-module.exports = {TEASPOON, TABLESPOON, OUNCE, CUP, PINT, QUART, GALLON, FOOT, INCH}
-
-
-
-
-
+module.exports = {
+  TEASPOON,
+  TABLESPOON,
+  OUNCE,
+  CUP,
+  PINT,
+  QUART,
+  GALLON,
+  FOOT,
+  INCH,
+};
